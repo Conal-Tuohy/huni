38f180227a963b2bc278c5a5ed3d371fc5220f1c
rrothwell <rrothwell@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2012-11-19 06:46:52 +0000

    RR: refactor and add some tree items

--- HUNI/trunk/VL_GWT_prototype/src/au/edu/versi/huni/gwt/client/VirtualLaboratory.java
+++ HUNI/trunk/VL_GWT_prototype/src/au/edu/versi/huni/gwt/client/VirtualLaboratory.java
@@ -27,6 +27,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.TextBoxBase;
 import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.VerticalSplitPanel;
@@ -121,51 +122,58 @@ public class VirtualLaboratory implements EntryPoint {
 		InlineLabel searchInlineLabel = new InlineLabel("Search:");
 		searchInlineLabel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
 		searchInlineLabel.setSize("200", "12");
+		layoutPanel.add(searchInlineLabel);
+
 		layoutPanel.setWidgetLeftWidth(searchInlineLabel, 12.0, Unit.PX, 45.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(searchInlineLabel, 3.0, Unit.PX, 18.0, Unit.PX);
-		layoutPanel.add(searchInlineLabel);
-		
+
 		TextBox searchTextBox = new TextBox();
 		searchTextBox.setTextAlignment(TextBoxBase.ALIGN_RIGHT);
 		searchTextBox.setText("for dataset by name");
 		searchTextBox.setName("searchForDataset");
 		searchTextBox.setAlignment(TextAlignment.LEFT);
 		searchTextBox.setSize("100", "12px");
+		layoutPanel.add(searchTextBox);
+
 		layoutPanel.setWidgetLeftWidth(searchTextBox, 57.0, Unit.PX, 173.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(searchTextBox, 0.0, Unit.PX, 24.0, Unit.PX);
-		layoutPanel.add(searchTextBox);
-		
+
 		InlineHyperlink advanceSearchHyperlink = new InlineHyperlink("Advanced search", false, "newHistoryToken");
+		layoutPanel.add(advanceSearchHyperlink);
+
 		layoutPanel.setWidgetLeftWidth(advanceSearchHyperlink, 236.0, Unit.PX, 101.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(advanceSearchHyperlink, 3.0, Unit.PX, 18.0, Unit.PX);
-		layoutPanel.add(advanceSearchHyperlink);
-		
+
 		InlineHyperlink registerHyperlink = new InlineHyperlink("Register", false, "newHistoryToken");
-		layoutPanel.setWidgetLeftWidth(registerHyperlink, 599.0, Unit.PX, 48.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(registerHyperlink, 3.0, Unit.PX, 18.0, Unit.PX);
 		layoutPanel.add(registerHyperlink);
 		
+		layoutPanel.setWidgetLeftWidth(registerHyperlink, 599.0, Unit.PX, 48.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(registerHyperlink, 3.0, Unit.PX, 18.0, Unit.PX);
+
 		InlineHyperlink loginHyperlink = new InlineHyperlink("Login/AAF login", false, "newHistoryToken");
+		layoutPanel.add(loginHyperlink);
+
 		layoutPanel.setWidgetLeftWidth(loginHyperlink, 664.0, Unit.PX, 92.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(loginHyperlink, 3.0, Unit.PX, 18.0, Unit.PX);
-		layoutPanel.add(loginHyperlink);
-		
+
 		InlineLabel virtualLaboratoryInlineLabel = new InlineLabel("Humanities Virtual Laboratory");
 		virtualLaboratoryInlineLabel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
 		virtualLaboratoryInlineLabel.setStyleName("headerName");
 		virtualLaboratoryInlineLabel.setSize("200", "50");
+		
 		headerDockLayoutPanel.addWest(virtualLaboratoryInlineLabel, 30.7);
 		
-		LayoutPanel logoContainerPanel = new LayoutPanel();
-		headerDockLayoutPanel.addEast(logoContainerPanel, 12.5);
-		
+		LayoutPanel logoContainerPanel = new LayoutPanel();		
 		Image logoImage = new Image("resources/images/HuNI_Logo_ICON.jpg");
 		logoImage.setAltText("HuNI Logo");
 		logoImage.setSize("150px", "91px");
-		logoContainerPanel.setWidgetLeftWidth(logoImage, 12.0, Unit.PX, 150.0, Unit.PX);
-		logoContainerPanel.setWidgetTopHeight(logoImage, 0.0, Unit.PX, 91.0, Unit.PX);
 		//image.getElement().getStyle().setPosition(Position.RELATIVE);
 		logoContainerPanel.add(logoImage);
+
+		logoContainerPanel.setWidgetLeftWidth(logoImage, 12.0, Unit.PX, 150.0, Unit.PX);
+		logoContainerPanel.setWidgetTopHeight(logoImage, 0.0, Unit.PX, 91.0, Unit.PX);
+
+		headerDockLayoutPanel.addEast(logoContainerPanel, 12.5);
 
 		return headerDockLayoutPanel;
 	}
@@ -199,6 +207,13 @@ public class VirtualLaboratory implements EntryPoint {
 		projectsDatasourcesDecoratorPanel.setWidget(projectsDatasourcesTree);
 		projectsDatasourcesTree.setSize("100%", "190px");
 		projectsDatasourcesDockLayoutPanel.add(projectsDatasourcesDecoratorPanel);
+		
+		TreeItem rootItem = new TreeItem();
+		rootItem.addTextItem("Project 0");
+		rootItem.addTextItem("Project 1");
+		rootItem.addTextItem("Project 2");
+
+		projectsDatasourcesTree.addItem(rootItem);
 		
 		return projectsDatasourcesDockLayoutPanel;
 	}
