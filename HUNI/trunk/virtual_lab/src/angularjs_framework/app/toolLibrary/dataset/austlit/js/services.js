91b95b9825b6ed932f5cdff14439ab2436c6912d
rrothwell <rrothwell@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2013-03-07 06:25:18 +0000

    RR: loading tool via the OpenSocial container.
    -- created a number of dummy tools to demonstrate tool selection in the UI.

---
+++ HUNI/trunk/virtual_lab/src/angularjs_framework/app/toolLibrary/dataset/austlit/js/services.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* Services */
+
+angular.module('austlitToolServices0', ['ngResource']).
+    factory('Page0', function($resource){
+  return null;
+});
+
+angular.module('austlitToolServices1', ['ngResource']).
+    factory('Page1', function($resource){
+    	  return null;
+});
+
+angular.module('austlitToolServices2', ['ngResource']).
+    factory('Page2', function($resource){
+    	  return null;
+});
---
+++ HUNI/trunk/virtual_lab/src/angularjs_framework/app/toolLibrary/search/search-fulltext/js/services.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* Services */
+
+angular.module('searchFullTextToolServices0', ['ngResource']).
+    factory('Page0', function($resource){
+  return null;
+});
+
+angular.module('searchFullTextToolServices1', ['ngResource']).
+    factory('Page1', function($resource){
+    	  return null;
+});
+
+angular.module('searchFullTextToolServices2', ['ngResource']).
+    factory('Page2', function($resource){
+    	  return null;
+});
