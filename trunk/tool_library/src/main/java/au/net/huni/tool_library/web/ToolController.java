9793bed00bd419151eaab899058b1fc69bff3353
richard.rothwell@versi.edu.au <richard.rothwell@versi.edu.au@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2013-05-06 05:17:29 +0000

    RR: upload of tools experiment.2

--- trunk/tool_library/src/main/java/au/net/huni/tool_library/web/ToolController.java
+++ trunk/tool_library/src/main/java/au/net/huni/tool_library/web/ToolController.java
@@ -9,10 +9,13 @@ import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
 import au.net.huni.tool_library.model.Tool;
 
@@ -21,21 +24,28 @@ import au.net.huni.tool_library.model.Tool;
 @RooWebScaffold(path = "tools", formBackingObject = Tool.class)
 public class ToolController {
 
+	@InitBinder
+    public void initBinder(WebDataBinder binder)
+    {
+        binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
+    }
+
     @RequestMapping(method = RequestMethod.POST, produces = "text/html")
     public String create(@Valid Tool tool, BindingResult bindingResult, Model uiModel,
-    		@RequestParam("content") CommonsMultipartFile content,
+    		@RequestParam("gadgetSpec") MultipartFile gadgetSpec,
     		HttpServletRequest httpServletRequest) {
+    	
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, tool);
             return "tools/create";
         }
 
-        File dest = new File("/tmp/" + content.getOriginalFilename());
+        File dest = new File("/tmp/" + gadgetSpec.getOriginalFilename());
         try {
-           content.transferTo(dest);
-           tool.setFilesize(content.getSize());
+        	gadgetSpec.transferTo(dest);
+           tool.setFilesize(gadgetSpec.getSize());
            tool.setFilepath(dest.getAbsolutePath());
-           tool.setContentType(content.getContentType());
+           tool.setContentType(gadgetSpec.getContentType());
         } catch (Exception e) {
            e.printStackTrace();
            return "mediauploads/create";
