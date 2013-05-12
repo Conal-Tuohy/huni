76dc854c5dc52243bdbc8f0328febbdd2a8d2b2d
richard.rothwell <richard.rothwell@versi.edu.au>
2013-05-12 03:29:19 +0000

    RR: adding RESTful web services for History --added tool parameter entity including tests and user interface

---
+++ virtual_lab/src/test/java/au/net/huni/model/ToolParameterDataOnDemand.java
@@ -0,0 +1,7 @@
+package au.net.huni.model;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = ToolParameter.class)
+public class ToolParameterDataOnDemand {
+}
---
+++ virtual_lab/src/test/java/au/net/huni/model/ToolParameterIntegrationTest.java
@@ -0,0 +1,12 @@
+package au.net.huni.model;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = ToolParameter.class)
+public class ToolParameterIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
