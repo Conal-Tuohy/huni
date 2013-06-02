4a8d6f8ab2915ea462d2e6c045e57753ea4bd010
richard.rothwell <richard.rothwell@versi.edu.au>
2013-06-02 02:06:19 +0000

    RR: added search pages for places, objects and events.

--- virtual_lab/src/main/webapp/js/huniaggregate/app.js
+++ virtual_lab/src/main/webapp/js/huniaggregate/app.js
@@ -7,6 +7,9 @@ var personSearch = angular.module('huniaggregate', [
 	'pagination']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/familyname-search', {templateUrl: 'partials/huniaggregate/people/familyname.html', controller: PersonSearchCtrl}).
+	  when('/familyname-search', {templateUrl: 'partials/huniaggregate/people/familyname.html', controller: PersonSearchCtrl}).
+	  when('/placename-search', {templateUrl: 'partials/huniaggregate/places/placename.html', controller: PlaceSearchCtrl}).
+	  when('/objectname-search', {templateUrl: 'partials/huniaggregate/objects/objectname.html', controller: ObjectSearchCtrl}).
+	  when('/eventname-search', {templateUrl: 'partials/huniaggregate/events/eventname.html', controller: EventSearchCtrl}).
       otherwise({redirectTo: '/familyname-search'});
 }]);
--- virtual_lab/src/main/webapp/js/huniaggregate/controllers.js
+++ virtual_lab/src/main/webapp/js/huniaggregate/controllers.js
@@ -20,6 +20,24 @@ var personSparqlTemplate =
 	+     'OPTIONAL {?person cidoc:P2_has_type / skos:prefLabel ?typeName}'
 	+ ' }'
 	;
+
+var placeSparqlTemplate = 	 
+	'PREFIX cidoc: <http://erlangen-crm.org/current/>'
+	+ ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>'
+	+ ' PREFIX foaf: <http://xmlns.com/foaf/0.1/>'
+	+ ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#>'
+	+ ' SELECT ?person ?firstName ?lastName ?birthDate ?typeName'
+	+ ' WHERE'
+	+ ' {'
+	+     '?person a cidoc:E21_Person .'
+	+     '?person foaf:firstName ?firstName .'
+	+     '?person foaf:lastName ?lastName .'
+	+     'FILTER(?lastName = "{{surName}}") .'
+	+     'OPTIONAL {?person cidoc:P98i_was_born / cidoc:P4_has_time-span / rdf:value ?birthDate}'
+	+     'OPTIONAL {?person cidoc:P2_has_type / skos:prefLabel ?typeName}'
+	+ ' }'
+	;
+
 var pagingSparqlTemplate =	
 	' OFFSET {{queryOffset}} LIMIT {{queryLimit}}';
 
@@ -57,4 +75,102 @@ function PersonSearchCtrl($scope, $routeParams, SparqlSearch) {
 	};	  
 }
 
-//PersonSearchCtrl.$inject = ['$scope', '$routeParams', 'PersonSearch'];
+//PersonSearchCtrl.$inject = ['$scope', '$routeParams', 'SparqlSearch'];
+
+//------------------------------------
+
+function PlaceSearchCtrl($scope, $routeParams, SparqlSearch) {
+
+	  $scope.noOfPages = 5;
+	  $scope.currentPage = pageIndex + 1;
+	  $scope.maxSize = 5;
+
+	$scope.placesByPlaceName = function(output) {
+		var placeName = $scope.placeName;
+		
+		// Populate the export button URL
+		var sparqlTemplate = placeSparqlTemplate;
+		var sparqlStr = sparqlTemplate.replace(/{{placeName}}/, placeName);
+		$scope.exportUrl = serviceURL + '?query=' + encodeURIComponent(sparqlStr) + '&output=csv';
+		
+		// Perform the request to populate the HTML results table.
+		sparqlTemplate = placeSparqlTemplate + pagingSparqlTemplate;
+		var queryOffset = queryLimit * pageIndex;
+		sparqlStr = sparqlTemplate.replace(/{{placeName}}/, placeName)
+			.replace(/{{queryOffset}}/, queryOffset + "")
+			.replace(/{{queryLimit}}/, queryLimit + "");
+		$scope.response = SparqlSearch.query({'query' : sparqlStr, 'output' : 'json'});
+	};
+	
+	$scope.turnPage = function (pageNo) {
+		pageIndex = pageNo - 1;
+		$scope.placesByPlaceName();
+	};	  
+}
+
+//PlaceSearchCtrl.$inject = ['$scope', '$routeParams', 'SparqlSearch'];
+
+//------------------------------------
+
+function ObjectSearchCtrl($scope, $routeParams, SparqlSearch) {
+
+	  $scope.noOfPages = 5;
+	  $scope.currentPage = pageIndex + 1;
+	  $scope.maxSize = 5;
+
+	$scope.objectsByObjectName = function(output) {
+		var objectName = $scope.objectName;
+		
+		// Populate the export button URL
+		var sparqlTemplate = objectSparqlTemplate;
+		var sparqlStr = sparqlTemplate.replace(/{{objectName}}/, objectName);
+		$scope.exportUrl = serviceURL + '?query=' + encodeURIComponent(sparqlStr) + '&output=csv';
+		
+		// Perform the request to populate the HTML results table.
+		sparqlTemplate = objectSparqlTemplate + pagingSparqlTemplate;
+		var queryOffset = queryLimit * pageIndex;
+		sparqlStr = sparqlTemplate.replace(/{{objectName}}/, objectName)
+			.replace(/{{queryOffset}}/, queryOffset + "")
+			.replace(/{{queryLimit}}/, queryLimit + "");
+		$scope.response = SparqlSearch.query({'query' : sparqlStr, 'output' : 'json'});
+	};
+	
+	$scope.turnPage = function (pageNo) {
+		pageIndex = pageNo - 1;
+		$scope.objectsByObjectName();
+	};	  
+}
+
+//ObjectSearchCtrl.$inject = ['$scope', '$routeParams', 'SparqlSearch'];
+//------------------------------------
+
+function EventSearchCtrl($scope, $routeParams, SparqlSearch) {
+
+	  $scope.noOfPages = 5;
+	  $scope.currentPage = pageIndex + 1;
+	  $scope.maxSize = 5;
+
+	$scope.eventsByEventName = function(output) {
+		var eventName = $scope.eventName;
+		
+		// Populate the export button URL
+		var sparqlTemplate = eventSparqlTemplate;
+		var sparqlStr = sparqlTemplate.replace(/{{eventName}}/, eventName);
+		$scope.exportUrl = serviceURL + '?query=' + encodeURIComponent(sparqlStr) + '&output=csv';
+		
+		// Perform the request to populate the HTML results table.
+		sparqlTemplate = eventSparqlTemplate + pagingSparqlTemplate;
+		var queryOffset = queryLimit * pageIndex;
+		sparqlStr = sparqlTemplate.replace(/{{eventName}}/, eventName)
+			.replace(/{{queryOffset}}/, queryOffset + "")
+			.replace(/{{queryLimit}}/, queryLimit + "");
+		$scope.response = SparqlSearch.query({'query' : sparqlStr, 'output' : 'json'});
+	};
+	
+	$scope.turnPage = function (pageNo) {
+		pageIndex = pageNo - 1;
+		$scope.eventsByEventName();
+	};	  
+}
+
+//EventSearchCtrl.$inject = ['$scope', '$routeParams', 'SparqlSearch'];
