ff36930c34651d449f269667ef200e89e1353621
richard.rothwell@versi.edu.au <richard.rothwell@versi.edu.au@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2013-06-02 00:05:23 +0000

    RR: minor tweaks. -- some comments -- moved JS to bottom of page to improve load times -- improved link colours in entry rectangles

--- trunk/virtual_lab/src/main/webapp/js/search/person/services.js
+++ trunk/virtual_lab/src/main/webapp/js/search/person/services.js
@@ -1,9 +1,9 @@
 'use strict';
 
+// Path to SPARQL end-point. Global since its needed elsewhere.
 var serviceURL = 'http://corbicula.huni.net.au/dataset/query';
 
 /* Services */
-
 
 angular.module('sparqlServices', ['ngResource']).
     factory('SparqlSearch', function($resource){
