6f7c6c1d1c342be6fe97c7357824c1459cb55642
richard.rothwell@versi.edu.au <richard.rothwell@versi.edu.au@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2013-06-04 07:04:36 +0000

    RR: a version of a person records page using AngularJS

--- trunk/virtual_lab/src/main/webapp/js/huniaggregate/controllers.js
+++ trunk/virtual_lab/src/main/webapp/js/huniaggregate/controllers.js
@@ -4,7 +4,7 @@
 
 angular.module('pagination', ['ui.bootstrap']);
 
-var personSparqlTemplate = 	 
+var peopleSparqlTemplate = 	 
 	'PREFIX cidoc: <http://erlangen-crm.org/current/>'
 	+ ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>'
 	+ ' PREFIX foaf: <http://xmlns.com/foaf/0.1/>'
@@ -21,7 +21,44 @@ var personSparqlTemplate =
 	+ ' }'
 	;
 
-var placeSparqlTemplate = 	 
+
+	var personSparqlTemplate =
+		'PREFIX cidoc: <http://erlangen-crm.org/current/>'
+		+ ' PREFIX foaf: <http://xmlns.com/foaf/0.1/>'
+		+ ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#>'
+		+ ' PREFIX oa: <http://www.openannotation.org/ns/>'
+		+ ' SELECT ?s ?p ?o ?g ?resource'
+		+ ' WHERE'
+		+ ' {'
+		+ '		BIND (<{{personId}}> AS ?resource) .'
+		+ '		{'
+		+ '			GRAPH ?g'
+		+ '			{ '
+		+ '				{'
+		+ '					?s ?p ?o.'
+		+ '					FILTER( ?s = <{{personId}}> || ?o = <{{personId}}> )'
+		+ '				}'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:P98i_was_born ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:P100i_died_in ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:P98i_was_born / cidoc:P4_has_time-span ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:P100i_died_in / cidoc:P4_has_time-span ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:cidoc:P95i_was_formed_by ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:P99i_was_dissolved_by ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:cidoc:P95i_was_formed_by / cidoc:P4_has_time-span ?s }'
+		+ '				UNION { ?s ?p ?o. <{{personId}}> cidoc:P99i_was_dissolved_by / cidoc:P4_has_time-span ?s }'
+		+ '			}'
+		+ '		}'
+		+ '		UNION'
+		+ '		{'
+		+ ' 		<{{personId}}> cidoc:P2_has_type ?s graph ?g'
+		+ ' 		{ ?s ?p ?o }'
+		+ ' 	}'
+		+ ' }'
+		+ ' LIMIT 10000'
+		;
+
+	
+	var placeSparqlTemplate = 	 
 	'PREFIX cidoc: <http://erlangen-crm.org/current/>'
 	+ ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>'
 	+ ' PREFIX foaf: <http://xmlns.com/foaf/0.1/>'
@@ -89,12 +126,12 @@ function PersonSearchCtrl($scope, $routeParams, SparqlSearch) {
 		var surName = $scope.familyName;
 		
 		// Populate the export button URL
-		var sparqlTemplate = personSparqlTemplate;
+		var sparqlTemplate = peopleSparqlTemplate;
 		var sparqlStr = sparqlTemplate.replace(/{{surName}}/, surName);
 		$scope.exportUrl = serviceURL + '?query=' + encodeURIComponent(sparqlStr) + '&output=csv';
 		
 		// Perform the request to populate the HTML results table.
-		sparqlTemplate = personSparqlTemplate + pagingSparqlTemplate;
+		sparqlTemplate = peopleSparqlTemplate + pagingSparqlTemplate;
 		var queryOffset = queryLimit * pageIndex;
 		sparqlStr = sparqlTemplate.replace(/{{surName}}/, surName)
 			.replace(/{{queryOffset}}/, queryOffset + "")
@@ -106,6 +143,104 @@ function PersonSearchCtrl($scope, $routeParams, SparqlSearch) {
 		pageIndex = pageNo - 1;
 		$scope.peopleByFamilyName();
 	};	  
+}
+
+//PersonSearchCtrl.$inject = ['$scope', '$routeParams', 'SparqlSearch'];
+
+//------------------------------------
+
+function PersonRecordCtrl($scope, $routeParams, SparqlSearch) {
+
+	var sparqlStr = personSparqlTemplate.replace(/{{personId}}/g, $routeParams.personId);
+	$scope.response = SparqlSearch.query({'query' : sparqlStr, 'output' : 'json'}, function(response) {	
+			var person = {};
+			person.occupations = [];
+			person.firstNames = [];
+			person.primaryTopics = [];
+			var tripleCount = response.results.bindings.length;
+			for (var tripleIndex0 = 0; tripleIndex0 < tripleCount; tripleIndex0++) {
+				var currentTriple0 = response.results.bindings[tripleIndex0];
+				// Locate a person identifier
+				if (currentTriple0.o.value == 'http://erlangen-crm.org/current/E21_Person') {
+					var personSubject = currentTriple0.s.value;
+					var originalRecordId = currentTriple0.g.value;
+					person.originalRecordId = originalRecordId;
+					for (var tripleIndex1 = 0; tripleIndex1 < tripleCount; tripleIndex1++) {
+						var currentTriple1 = response.results.bindings[tripleIndex1];
+						if (currentTriple1.s.value == personSubject) {
+							// Navigate to the first name associated with the identifier
+							if (currentTriple1.p.value == 'http://xmlns.com/foaf/0.1/firstName') {
+								var firstName = currentTriple1.o.value;
+								person.firstNames.push(firstName);
+							}
+							// Navigate to the last name associated with the identifier
+							if (currentTriple1.p.value == 'http://xmlns.com/foaf/0.1/lastName') {
+								person.lastName = currentTriple1.o.value;
+							}
+							// Navigate to the birth date associated with the person identifier							
+							if (currentTriple1.p.value == 'http://erlangen-crm.org/current/P98i_was_born') {
+								var birthObject = currentTriple1.o.value;
+								for (var tripleIndex2 = 0; tripleIndex2 < tripleCount; tripleIndex2++) {
+									var currentTriple2 = response.results.bindings[tripleIndex2];
+									if (currentTriple2.s.value == birthObject) {
+										if (currentTriple2.p.value == 'http://erlangen-crm.org/current/P4_has_time-span') {
+											var timeSpanObject = currentTriple2.o.value;
+											for (var tripleIndex3 = 0; tripleIndex3 < tripleCount; tripleIndex3++) {
+												var currentTriple3 = response.results.bindings[tripleIndex3];
+												if (currentTriple3.s.value == timeSpanObject) {
+													if (currentTriple3.p.value == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value') {
+														person.birthDate = currentTriple3.o.value;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+							if (currentTriple1.p.value == 'http://erlangen-crm.org/current/P100i_died_in') {
+								var deathObject = currentTriple1.o.value;
+								for (var tripleIndex5 = 0; tripleIndex5 < tripleCount; tripleIndex5++) {
+									var currentTriple5 = response.results.bindings[tripleIndex5];
+									if (currentTriple5.s.value == deathObject) {
+										if (currentTriple5.p.value == 'http://erlangen-crm.org/current/P4_has_time-span') {
+											var timeSpanObject = currentTriple5.o.value;
+											for (var tripleIndex6 = 0; tripleIndex6 < tripleCount; tripleIndex6++) {
+												var currentTriple6 = response.results.bindings[tripleIndex6];
+												if (currentTriple6.s.value == timeSpanObject) {
+													if (currentTriple6.p.value == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value') {
+														person.deathDate = currentTriple6.o.value;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+							// Navigate to the multiple occupations associated with the person identifier
+							if (currentTriple1.p.value == 'http://erlangen-crm.org/current/P2_has_type') {
+								var occupationObject = currentTriple1.o.value;
+								for (var tripleIndex4 = 0; tripleIndex4 < tripleCount; tripleIndex4++) {
+									var currentTriple4 = response.results.bindings[tripleIndex4];
+									if (currentTriple4.s.value == occupationObject) {
+										if (currentTriple4.p.value == 'http://www.w3.org/2004/02/skos/core#prefLabel') {
+											var occupation = currentTriple4.o.value;
+											person.occupations.push(occupation);
+										}
+									}
+								}
+							}
+							// Navigate to the books etc. associated with the person identifier
+							if (currentTriple1.p.value == 'http://xmlns.com/foaf/0.1/isPrimaryTopicOf') {
+								var primaryTopic = currentTriple1.o.value;
+								person.primaryTopics.push(primaryTopic);
+							}
+						}
+					}
+				}
+			}
+			response.length = 0;
+			response.person = person;
+		});	
 }
 
 //PersonSearchCtrl.$inject = ['$scope', '$routeParams', 'SparqlSearch'];
