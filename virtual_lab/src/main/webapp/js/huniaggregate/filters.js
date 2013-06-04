ca50b75247bdf71a3c12fbec5f0707e8d4efe2ae
richard.rothwell <richard.rothwell@versi.edu.au>
2013-06-04 07:04:36 +0000

    RR: a version of a person records page using AngularJS

--- virtual_lab/src/main/webapp/js/huniaggregate/filters.js
+++ virtual_lab/src/main/webapp/js/huniaggregate/filters.js
@@ -2,3 +2,10 @@
 
 /* Filters */
 
+angular.module('hexEncodeModule', []).
+	filter('hexEncode', function() {
+		return function(input) {
+			var output = encodeURIComponent(input);
+			return output.replace('.', '%2E');
+	};
+});
