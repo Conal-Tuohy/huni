18f7d0e313f31248fe440ba1973057f08f828161
richard.rothwell <richard.rothwell@versi.edu.au>
2013-06-09 01:11:23 +0000

    RR: bacl-copied html changes to angulars_framework folders.

---
+++ virtual_lab/src/angularjs_framework/app/js/huniaggregate/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* App Module */
+
+var personSearch = angular.module('huniaggregate', [
+	'sparqlServices',
+	'pagination',
+	'hexEncodeModule']).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when('/familyname-search', {templateUrl: 'partials/huniaggregate/people/familyname.html', controller: PersonSearchCtrl}).
+	when('/person', {templateUrl: 'partials/huniaggregate/people/person.html', controller: PersonRecordCtrl}).
+	when('/placename-search', {templateUrl: 'partials/huniaggregate/places/placename.html', controller: PlaceSearchCtrl}).
+	when('/objectname-search', {templateUrl: 'partials/huniaggregate/objects/objectname.html', controller: ObjectSearchCtrl}).
+	when('/eventname-search', {templateUrl: 'partials/huniaggregate/events/eventname.html', controller: EventSearchCtrl}).
+	otherwise({redirectTo: '/familyname-search'});
+}]);
