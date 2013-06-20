aac45a2b73a56883bf3b2ecf1a53e4fca53c44ce
richard.rothwell@versi.edu.au <richard.rothwell@versi.edu.au@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2013-06-20 23:13:02 +0000

    RR: disabled test examples. -- there is a lot to learn before I can apply this properly to the existing Marco code.

--- trunk/virtual_lab_js/test/unit/controllersSpec.js
+++ trunk/virtual_lab_js/test/unit/controllersSpec.js
@@ -2,15 +2,53 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+//describe('controllers', function(){
+//  beforeEach(module('myApp.controllers'));
+//
+//
+//  it('should ....', inject(function() {
+//    //spec body
+//  }));
+//
+//  it('should ....', inject(function() {
+//    //spec body
+//  }));
+//});
 
+describe('Simple Search controllers', function(){
+	
+	var $scope = {
+			$on: function() {
+				
+			}
+	}
+	
+	var solrSearchService= {
+			getFacetResults : function() {
+				return {
+					prov_site_long: [0, 1, 2, 3],
+					type: [0, 1, 2, 3, 4] 
+				}
+			},
+			faceton: function() {
+	            $rootScope.$broadcast('facetResultsUpdated');
+			}
+	}
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  beforeEach(function() {
+	  // Do nothing
+  });
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+//  it('should call the on facetResultsUpdated event handler and set nproviders to 4', inject(function($rootScope, $controller) {
+//	  var scope = $rootScope.$new();
+//	  var ctrl = $controller('DocumentStatisticsCtrl', {$scope: scope});
+//	  ctrl.init();
+//	  expect(scope.nproviders).toBe(4);
+//	  //expect(scope.nentity_types).toBe(5);
+//  }));
+
+//  it('should ....', inject(function() {
+//    //spec body
+//  }));
+  
 });
