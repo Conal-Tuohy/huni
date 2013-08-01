595843faf88e878f289e0b7dc01480f3abea5ae1
richard.rothwell <richard.rothwell@versi.edu.au>
2013-08-01 03:28:44 +0000

    RR: move to branches

--- data-store/corbicula/xproc/run-pipeline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Djava.util.logging.config.file=/opt/calabash-1.0.9-94/calabash-logging.properties -Xmx1000m -jar /opt/calabash-1.0.9-94/calabash.jar -D "$@"
--- tool_library/src/test/java/au/net/huni/tool_library/model/ToolDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package au.net.huni.tool_library.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Tool.class)
-public class ToolDataOnDemand {
-}
