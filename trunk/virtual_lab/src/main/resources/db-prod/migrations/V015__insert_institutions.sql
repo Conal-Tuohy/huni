2d4616b48b9185af41bbeacb2c7cd50c56c4e61b
richard.rothwell@versi.edu.au <richard.rothwell@versi.edu.au@7b4ef106-d63e-ecc7-c33c-958d4cff7a8c>
2013-08-19 01:58:51 +0000

    RR: DB migrations. -- debugging the process -- ToolCategory UI now works.

---
+++ trunk/virtual_lab/src/main/resources/db-prod/migrations/V015__insert_institutions.sql
@@ -0,0 +1,7 @@
+INSERT INTO "APP"."INSTITUTION" (ID,NAME,VERSION) VALUES 
+(1,'UNKNOWN',0),
+(2,'Unaffiliated',0),
+(3,'Deakin University',0),
+(4,'Monash University',0),
+(5,'RMIT University of Technology',0),
+(6,'University of Melbourne',0)
